d75765ebbd2f5969f0840a5baa31fdaf17711c39
Leticia Deodato <leticiamanuelletd@gmail.com>
2023-11-30 23:57:58 -0300

    add login routes

--- empire/lib/routes.dart
+++ empire/lib/routes.dart
@@ -1,8 +1,12 @@
-import 'package:empire/initial/initial_page.dart';
+import 'package:empire/modules/authentiction/login_email.dart';
+import 'package:empire/modules/authentiction/login_password.dart';
+import 'package:empire/modules/initial/initial_page.dart';
 import 'package:flutter/material.dart';
 
 class Routes{
   final Map<String, Widget Function(BuildContext)> routes = {
     '/': (_) => const InitialPage(),
+    '/login_email': (_) => const LoginEmail(),
+    '/login_password': (_) => const LoginPassword(),
   };
 }
