0792596183e9ec7db7372ea149a595139113bd8c
Leticia Deodato <leticiamanuelletd@gmail.com>
2023-12-02 15:16:58 -0300

    add sports page route

--- empire/lib/routes.dart
+++ empire/lib/routes.dart
@@ -2,6 +2,7 @@ import 'package:empire/modules/authentiction/login_email.dart';
 import 'package:empire/modules/authentiction/login_password.dart';
 import 'package:empire/modules/home/home_page.dart';
 import 'package:empire/modules/initial/initial_page.dart';
+import 'package:empire/modules/sports/sports_page.dart';
 import 'package:flutter/material.dart';
 
 class Routes{
@@ -10,5 +11,6 @@ class Routes{
     '/login_email': (_) => const LoginEmail(),
     '/login_password': (_) => const LoginPassword(),
     '/home': (_) => const HomePage(),
+    '/sports': (_) => const SportsPage(),
   };
 }
