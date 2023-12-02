b40ec2afa64e2f8152c23f19de7ba372268c2db4
Leticia Deodato <leticiamanuelletd@gmail.com>
2023-12-02 02:41:36 -0300

    card tip componentization

--- empire/lib/components/card_tip.dart
+++ empire/lib/components/card_tip.dart
@@ -2,42 +2,91 @@ import 'package:empire/components/button_icon.dart';
 import 'package:flutter/material.dart';
 
 class CardTip extends StatelessWidget {
-  const CardTip({super.key});
+  final String img;
+  
+  const CardTip({super.key, required this.img,});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Image(
-            height: 16,
-            width: 21,
-            image: AssetImage('lib/images/arrow_right.png'),
-            fit: BoxFit.cover),
-        Column(
-          children: [
-            Row(
-              children: [
-                ButtonIcon(
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+        boxShadow: [
+          BoxShadow(
+            color: const Color.fromARGB(255, 10, 10, 10).withOpacity(0.15),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      width: 275,
+      height: 340,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
+            ),
+            child: Image(
+                height: 141,
+                width: 275,
+                image: AssetImage(img),
+                fit: BoxFit.cover),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 20),
+                height: 140,
+                width: 150,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Apostas ao Vivo: Estratégias e Dicas para o Sucesso',
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text('Explore as melhores práticas ...'),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ButtonIcon(
                     onPressed: () {},
-                    colorBackground: Colors.transparent,
+                    colorBackground: const Color.fromARGB(255, 255, 255, 255),
                     colorText: Colors.black,
                     img: 'lib/images/soccer.png',
                     text: 'Futebol',
                     heightIcon: 16,
-                    widhtIcon: 16),
-                ButtonIcon(
+                    widhtIcon: 16,
+                    widhtButton: 80,
+                    heightButton: 29,
+                    textSize: 10,
+                  ),
+                  ButtonIcon(
                     onPressed: () {},
-                    colorBackground: Colors.transparent,
+                    colorBackground: Color.fromARGB(255, 255, 255, 255),
                     colorText: Colors.black,
                     img: 'lib/images/basket.png',
                     text: 'Basquete',
                     heightIcon: 16,
-                    widhtIcon: 16),
-              ],
-            )
-          ],
-        ),
-      ],
+                    widhtIcon: 16,
+                    widhtButton: 80,
+                    heightButton: 29,
+                    textSize: 10,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
