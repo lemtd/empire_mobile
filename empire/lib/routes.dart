370ac64ce25038ca21c6e8e200e4dd7ad0458dad
Leticia Deodato <leticiamanuelletd@gmail.com>
2023-12-03 23:14:10 -0300

    add matchpage route

--- empire/lib/routes.dart
+++ empire/lib/routes.dart
@@ -2,15 +2,17 @@ import 'package:empire/modules/authentiction/login_email.dart';
 import 'package:empire/modules/authentiction/login_password.dart';
 import 'package:empire/modules/home/home_page.dart';
 import 'package:empire/modules/initial/initial_page.dart';
+import 'package:empire/modules/match/match_page.dart';
 import 'package:empire/modules/sports/sports_page.dart';
 import 'package:flutter/material.dart';
 
-class Routes{
+class Routes {
   final Map<String, Widget Function(BuildContext)> routes = {
     '/': (_) => const InitialPage(),
     '/login_email': (_) => const LoginEmail(),
     '/login_password': (_) => const LoginPassword(),
     '/home': (_) => const HomePage(),
     '/sports': (_) => const SportsPage(),
+    '/match': (_) => const MatchPage(),
   };
 }
