b6b96649bc7d762c0111bc347c6d994fd80348e8
Leticia Deodato <leticiamanuelletd@gmail.com>
2023-12-04 16:42:33 -0300

    api: create requests to the login endpoint

--- empire/lib/modules/authentiction/authentication_controller.dart
+++ empire/lib/modules/authentiction/authentication_controller.dart
@@ -1,3 +1,4 @@
+import 'package:empire/src/stores/login_store.dart';
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 
@@ -6,7 +7,9 @@ part 'authentication_controller.g.dart';
 class AuthenticationController = _AuthenticationControllerBase with _$AuthenticationController;
 
 abstract class _AuthenticationControllerBase with Store {
-  _AuthenticationControllerBase();
+  final LoginStore _loginStore;
+  
+  _AuthenticationControllerBase(this._loginStore);
 
   @observable
   TextEditingController email = TextEditingController();
@@ -20,6 +23,11 @@ abstract class _AuthenticationControllerBase with Store {
   final keyboardEmail = TextInputType.emailAddress;
 
   final keyboardPassword = TextInputType.visiblePassword;
+
+  Future<bool> loginUser() async {
+    final success = await _loginStore.loginUser();
+    return success;
+  }
 
   // @action
   // void changeValue() {
