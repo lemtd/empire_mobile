936b2a216ffb9e0fe8838c1713bdb9689f30d8f8
Leticia Deodato <leticiamanuelletd@gmail.com>
2023-12-04 16:42:04 -0300

    api: create requests to the login endpoint

--- empire/lib/locator.dart
+++ empire/lib/locator.dart
@@ -1,10 +1,19 @@
 import 'package:empire/modules/authentiction/authentication_controller.dart';
 import 'package:empire/routes.dart';
+import 'package:empire/src/repositories/login_repository.dart';
+import 'package:empire/src/states/login_state.dart';
+import 'package:empire/src/stores/login_store.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
 setupLocator() {
   getIt.registerLazySingleton<Routes>(() => Routes());
-  getIt.registerLazySingleton<AuthenticationController>(() => AuthenticationController());
+  getIt.registerLazySingleton<LoginRepository>(() => LoginRepository());
+  getIt.registerLazySingleton<LoginState>(() => LoginState());
+  getIt.registerLazySingleton<LoginStore>(() => LoginStore(
+        getIt<LoginState>(),
+        getIt<LoginRepository>(),
+      ));
+  getIt.registerLazySingleton<AuthenticationController>(() => AuthenticationController(getIt<LoginStore>()));
 }
