6abc8c7a7e2417de80d6c4fcaf663277f08bbdc8
Leticia Deodato <leticiamanuelletd@gmail.com>
2023-12-04 22:34:38 -0300

    add championships injection dependency

--- empire/lib/locator.dart
+++ empire/lib/locator.dart
@@ -1,12 +1,15 @@
 import 'package:empire/modules/authentiction/authentication_controller.dart';
 import 'package:empire/modules/initial/app_controller.dart';
 import 'package:empire/routes.dart';
+import 'package:empire/src/repositories/championship_repository.dart';
 import 'package:empire/src/repositories/login_repository.dart';
 import 'package:empire/src/repositories/match_repository.dart';
 import 'package:empire/src/repositories/sports_repository.dart';
+import 'package:empire/src/states/championship_state.dart';
 import 'package:empire/src/states/login_state.dart';
 import 'package:empire/src/states/match_state.dart';
 import 'package:empire/src/states/sports_state.dart';
+import 'package:empire/src/stores/championship_store.dart';
 import 'package:empire/src/stores/login_store.dart';
 import 'package:empire/src/stores/match_store.dart';
 import 'package:empire/src/stores/sports_store.dart';
@@ -36,8 +39,15 @@ setupLocator() {
         getIt<SportsState>(),
         getIt<SportsRepository>(),
       ));
+  getIt.registerLazySingleton<ChampionshipRepository>(() => ChampionshipRepository());
+  getIt.registerLazySingleton<ChampionshipState>(() => ChampionshipState());
+  getIt.registerLazySingleton<ChampionshipStore>(() => ChampionshipStore(
+        getIt<ChampionshipState>(),
+        getIt<ChampionshipRepository>(),
+      ));
   getIt.registerLazySingleton<AppController>(() => AppController(
         getIt<MatchStore>(),
         getIt<SportsStore>(),
+        getIt<ChampionshipStore>(),
       ));
 }
--- empire/lib/modules/initial/app_controller.dart
+++ empire/lib/modules/initial/app_controller.dart
@@ -1,3 +1,4 @@
+import 'package:empire/src/stores/championship_store.dart';
 import 'package:empire/src/stores/match_store.dart';
 import 'package:empire/src/stores/sports_store.dart';
 import 'package:mobx/mobx.dart';
@@ -9,12 +10,14 @@ class AppController = _AppControllerBase with _$AppController;
 abstract class _AppControllerBase with Store {
   final MatchStore matchStore;
   final SportsStore sportsStore;
+  final ChampionshipStore championshipStore;
   
-  _AppControllerBase(this.matchStore, this.sportsStore);
+  _AppControllerBase(this.matchStore, this.sportsStore, this.championshipStore);
 
   Future<void> init() async {
     await matchStore.fetchMatches();
     await sportsStore.fetchSports();
+    await championshipStore.fetchChampionships();
   }
 
   // @action
